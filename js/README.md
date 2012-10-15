f654f92dbbdf936d57351382636b09baa10aac34
Enrique Gamez <egamezf@gmail.com>
2012-10-14 21:45:02 -0500

    Updating jquery scripts. Options added to the plugin

--- js/README.md
+++ js/README.md
@@ -1,10 +1,75 @@
-inere [jQuery][1] plugins
+[jQuery][1] plugin para verificar la clave de el [R.F.C][2]
 ====
 
-Estos plugins son únicamentepara verificar la válidez de la clave del RFC.
+Este plugin fue creado para facilitar la captura de la clave del [R.F.C][2], es decir, ayudar a visualizar posibles errores en su captura. El método para lograrlo es a través de la comparación de el dígito verificador suministrado por la captura y uno que se calcula dentro de la función.
 
-Estos plugins están basados en el plugin [password_strength][2].
+Este plugin está basados en el plugin [password_strength][3] de Sagie Maoz.
 
+El procedimiento para verificar la clave se basa en la comparación de el dígito varificador (último carácter de la clave del [R.F.C][2]). Para esto, se raliza el calculo de el dígito verificador para después compararlo con el suministrado.
+
+El procedimiento para el calculo de este dígito está descrito en el [Instructivo para formación del Registro Federal de Contribuyentes][4].
+
+Uso
+---
+
+Para su uso únicamente es necesario llamar a la función para un campo de tipo texto:
+
+	$( '#rfc' ).digito_verificador();
+
+La función elimina cualquier espacio o guion antes de realizar el calculo de el dígito, de modo que la introducción, del [R.F.C][2] en el campo, puede incluir estos carácteres para mejorar su lectura.
+
+Resultados
+----------
+
+La función, por defecto, colorea en verde el campo cuando el dígito verificador suminstrado coincide con el calculado, en caso contrario el campo el coloreado con un fundo guinda.
+
+
+Opciones
+--------
+
+Las opciones disponibles por el momento son:
+* legendOnFail -- Cadena de carácteres para ser desplegados contiguos al campo de introducción del R.F.C cuando la clave introducida no coincida con el dígito calculado. El valor, por defecto, es una cadena vacia,
+* legendOnSuccess -- Cadena de carácteres cuando el dígito calculado coincida con el suministrado. El valor por defecto es una cadena vacia,
+* withColors -- Variable de tipo booleana para instruir si el campo de entrada del RFC cambiara de color segun el resultado de la verificación.
+
+Ejemplos
+--------
+
+Para desplegar las leyendas 'Captura correcta' y 'Posible error en la captura' sólo sera necesario declararlo en las opciones de la manera siguiente:
+
+	$('#rfc').digito_verificador({
+		legendOnSuccess: "Captura correcta.",
+		legendOnFail: "Posible error en la captura."
+	});
+
+es posible agregar a la cadena de instruccuines de el selector algunos atributos CSS, por ejemplo:
+
+	$('#rfc').digito_verificador({
+		legendOnSuccess: "Captura correcta.",
+		legendOnFail: "Posible error en la captura."
+	}).css('text-transform', 'uppercase');
+
+el ejemplo anterior agregara las leyendas junto al campo de introducción de la clave del [R.F.C][2] y cambiara de color el fondo del campo también, pero si se quiere desabilitar lo opción del cambio de color solo sera necesario agregar, a la opciones de arriba la opción 'withColors' con el valor 'false':
+
+	$('#rfc').digito_verificador({
+		legendOnSuccess: "Captura correcta.",
+		legendOnFail: "Posible error en la captura.",
+		withColors: false
+	}).css('text-transform', 'uppercase');
+
+Se provee también un ejemplo de uso para una página web simple (rfc.html).
+
+Autores
+-------
+LAE
+Enrique Gámez <egamezf@gmail.com>
+
+Licencia
+--------
+Licensed under the [GPL][5] license
 
 [1]: http://jquery.com/
-[2]: http://archive.plugins.jquery.com/project/password_strength
+[2]: http://www.sat.gob.mx/sitio_internet/21_12672.html
+[3]: http://archive.plugins.jquery.com/project/password_strength
+[4]: http://www.sisi.org.mx/jspsi/documentos/2006/seguimiento/06101/0610100107106_065.doc
+[5]: http://www.gnu.org/licenses/gpl-3.0.html
--- js/jquery.digito_verificador.js
+++ js/jquery.digito_verificador.js
@@ -2,35 +2,81 @@
  * Copyright (c) 2012	Lae,
  *			Enrique Gámez <egamez@edisson.com.mx>
  *
+ * The present plugin it was based on the work of
+ * Sagie Maoz <n0nick@php.net>
  * http://n0nick.net/jquery/password-strength/
- *
- * Password Strength (0.1.2)
- * by Sagie Maoz (n0nick.net)
- * n0nick@php.net
- *
- * This plugin will check the value of a password field and evaluate the
- * strength of the typed password. This is done by checking for
- * the diversity of character types: numbers, lowercase and uppercase
- * letters and special characters.
  *
  * Copyright (c) 2010 Sagie Maoz <n0nick@php.net>
  * Licensed under the GPL license, see http://www.gnu.org/licenses/gpl-3.0.html 
  *
+ * And some ideas on the work of Pawel Decowski,
+ * specially the function 'normalize'.
  *
- * NOTE: This script requires jQuery to work.  Download jQuery at www.jquery.com
+ * http://paweldecowski.github.com/jQuery-CreditCardValidator/
  *
  */
 (function($) {
 
-var digitoVerificador = new function()
-{
+var methods = {
 
-	this.obtenDigitoVerificador = function(rfc)
-	{
+	init: function(options) {
+		var settings = $.extend({
+			'legendOnFail': '',
+			'legendOnSuccess': '',
+			'withColors': true
+		}, options);
+
+		return this.each(function() {
+
+
+			$(this).bind('keyup.digito_verificador', function() {
+
+				var val = methods.normalize($(this).val().toUpperCase());
+				var bienformado = methods.rfcValido(val);
+				var container = null;
+
+				if ( settings.legendOnFail.length || settings.legendOnSuccess.length ) {
+					container = $('<span/>').attr('class', 'digito_verificador');
+					$(this).after(container);
+				}
+
+				if ( bienformado ) {
+					var legend = '';
+					var bg_color = '';
+					if ( methods.validaDigitoVerificador(val) ) {
+						legend = settings.legendOnSuccess;
+						bg_color = '#00CC00';
+					} else {
+						legend = settings.legendOnFail;
+						bg_color = '#990000';
+					}
+					if ( legend.length ) container.text(legend).attr('class', 'digito_verificador');
+					if ( settings.withColors ) {
+						$(this).css('color', '#fff')
+							.css('background', bg_color);
+					}
+
+				} else {
+					// This block will erase legends when the
+					// the RFC clave isn't complete
+					if ( settings.legendOnFail.length || settings.legendOnSuccess.length ) container.text('').attr('class', 'digito_verificador');
+					$(this).css('color', '');
+					$(this).css('background', '');
+				}
+			});
+
+			if ($(this).val() != '') { // thanks Jason Judge
+				$(this).trigger('keyup.digito_verificador');
+			}
+		});
+
+	},
+
+	obtenDigitoVerificador: function( rfc ) {
 		var anexo3 =	{0:0, 1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9,
 				A:10, B:11, C:12, D:13,   E:14, F:15, G:16, H:17, I:18, J:19,
 				K:20, L:21, M:22, N:23, '&':24, O:25, P:26, Q:27, R:28, S:29,
-				T:30, U:31, V:32, W:33,   X:34, Y:35, Z:36, ' ':37};
+				T:30, U:31, V:32, W:33,   X:34, Y:35, Z:36, ' ':37, \u00D1:38};
 		var current;
 		var suma = 0;
 		var acumulado = 0;
@@ -63,72 +109,38 @@ var digitoVerificador = new function()
 		else				digit = 11-remainder;
 
 		return digit;
-	};
+	},
 
-	this.rfcValido = function(rfc)
-	{
+	rfcValido: function( rfc ) {
 		var validRFC = true;
-		var regexp = /^([a-z]{3,4})([0-9]{2})[01][0-9][0-3][0-9]([0-9a-z]{2})[0-9a]$/i;
+		var regexp = /^([A-Z\u00D1]{3,4})([0-9]{2})((0[1-9])|(1[012]))((0[1-9])|([12][0-9])|(3[01]))([0-9A-Z]{2})[0-9Z]$/;
 		validRFC = regexp.test( rfc );
 		return validRFC;
-	};
+	},
 
-	this.validaDigitoVerificador = function(rfc)
-	{
-		var digito = this.obtenDigitoVerificador(rfc);
+	validaDigitoVerificador: function( rfc ) {
+		var digito = methods.obtenDigitoVerificador(rfc);
 		var pattern = /\w(?=$)/;
 		var supplied = pattern.exec(rfc);
-		return (digito == supplied) ? 1 : 0;
-	};
+		return (digito == supplied);
+	},
+
+	normalize: function( rfc ) {
+		return rfc.replace(/[ -]/g, '');
+	},
+
 };
 
-$.fn.digito_verificador = function(options)
-{
-	var settings = $.extend({
-		'minLength' : 12,
-		'maxLength' : 13,
-		'texts' : {
-			0: 'Dígito no concuerda',
-			1: 'Dígito idéntico'
-		},
-		'onCheck': null
-	}, options);
+$.fn.digito_verificador = function( method ) {
 
-	return this.each(function()
-	{
-		var container = null;
-		container = $('<span/>').attr('class', 'digito_verificador');
-		$(this).after(container);
+	if ( methods[method] ) {
+		return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+	} else if ( typeof method === 'object' || ! method ) {
+		return methods.init.apply( this, arguments );
+	} else {
+		$.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+	}
 
-		$(this).bind('keyup.digito_verificador', function()
-		{
-			var val = $(this).val().toUpperCase();
-			var bienformado = digitoVerificador.rfcValido(val);
-			if ( (val.length == 12 && bienformado) ||
-			     (val.length == 13 && bienformado) )
-			{
-				var level = digitoVerificador.validaDigitoVerificador(val);
-				$(this).text("digito: " + level + ".");
-				var _class = 'digito_verificador_' + level;
-
-				if (!container.hasClass(_class) && level in settings.texts)
-				{
-					container.text(settings.texts[level]).attr('class', 'digito_verificador ' + _class);
-				}
-			}
-			else
-			{
-				container.text('').attr('class', 'digito_verificador');
-			}
-			if ( settings.onCheck ) {
-				settings.onCheck.call(this, level);
-			}
-		});
-
-		if ($(this).val() != '') { // thanks Jason Judge
-			$(this).trigger('keyup.digito_verificador');
-		}
-	});
 };
 
 })(jQuery);
