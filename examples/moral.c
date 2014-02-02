4a68738e416dc897667d853b5cf75c7a014e963d
egamez <egamezf@gmail.com>
2014-02-02 13:15:48 -0600

    Adaptacion de cambios por nueva signatura de la funcion que calcula la clave

--- examples/moral.c
+++ examples/moral.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2013,  Lae
- *                      Enrique Gámez Flores <egamez@edisson.com.mx>,
+ * Copyright (c) 2013-2014, L3a,
+ *			    Enrique Gámez Flores <egamez@edisson.com.mx>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -37,12 +37,15 @@ main(int argc, char *argv[])
   int verbose = 0;
 
   if ( argc < 5 ) {
-    fprintf(stderr, "Sintax:\n\n\t%s <denominacion-social> <ano> <mes> <dia>\n\n", argv[0]);
+    fprintf(stderr, "Sintax:\n\n\t%s <denominacion-social> <ano> <mes> <dia> [<tipo-de-sociedad>]\n\n", argv[0]);
     return 1;
   }
 
   memset(clave, 0, 13);
-  clave_rfc_persona_moral(clave, argv[1], argv[2], argv[3], argv[4], verbose);
+  if ( argc == 5 )
+    clave_rfc_persona_moral(clave, argv[1], NULL, argv[2], argv[3], argv[4], verbose);
+  else
+    clave_rfc_persona_moral(clave, argv[1], argv[5], argv[2], argv[3], argv[4], verbose);
 
   printf("Clave del R.F.C.: %s\n", clave);
 
