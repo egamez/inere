931390b9ba88fdb1ffb63c58c69d4ce6c018b0db
Enrique Gámez <egamezf@gmail.com>
2015-02-16 19:48:57 -0600

    Se agregaron opciones para modificar la representacion desde la linea de comando

--- cfdi/src/ri.c
+++ cfdi/src/ri.c
@@ -1,34 +1,214 @@
+/*
+ * Copyright (c) 2015, L3a,
+ *                     Enrique Gamez Flores <egamez@edisson.com.mx>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 #ifndef INERE_CFDI_R12NIMPRESA_H_
 #include "inere/cfdi/r12nimpresa.h"
 #endif
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <getopt.h>
 
 /**
  * http://stackoverflow.com/questions/5309471/getting-file-extension-in-c
  */
 
+/* Forward declaration */
+void usage(void);
+
+void
+usage(void)
+{
+  printf("Uso: ri [OPCIONES] CFDI [OUTPUT]\n\n");
+  printf("\
+Programa para generar la representación impresa (archivo PDF) a partir de un\n\
+CFDI.\n\n");
+
+  printf("\
+Principales opciones:\n\
+\n\
+  -b BANNER, --banner=BANNER	Leyenda a aparecer como encabezado de la\n\
+				representación impresa.\n\
+  -i INFO,   --info=INFO	Mensaje a aparecer como información, por\n\
+				ejemplo: Teléfono, email, etc.\n\
+  -f PATH,   --font=PATH	Ruta en donde se encuentran las TTF fonts\n\
+				con las que se generará la representación\n\
+				impresa, diferente a las TTF enunciadas al\n\
+				momento de compilación.\n");
+  printf("\
+  -F PATH, --font-bold=PATH	Ruta en donde se encuentran las TTF fonts\n\
+				en negritas con las que se generará la\n\
+				representación impresa, diferente a las TTF\n\
+				enunciadas al momento de compilación.\n\
+  -s SIZE, --font-size=SIZE	Tamaño de la font, en dpi, con la que se\n\
+				generará la representación impresa. El\n\
+				tamaño por default es 8 (dpi).\n");
+  printf("\
+  -l SIZE, --font-label-size=SIZE Tamaño de la font, en dpi, con la que se\n\
+				generarán las etiqueta de la información de\n\
+				la representación impresa. El tamaño por\n\
+				default es 8 (dpi).\n\
+  -v,  --verbose		Imprime mensajes extra acerca de la ejecución\n\
+				en la generación.\n\
+  -h,  --help			Imprime este mensaje.\n");
+  printf("\
+\n\n\
+Bugs a <egamezf@gmail.com>\n");
+
+}
+
+/**
+ *
+ */
 int
 main(int argc, char *argv[])
 {
   int res = 0;
-  int verbose = 0;
+  int ch = 0;
+  const char *banner = NULL;
+  const char *info = NULL;
+  const char *font_path = NULL;
+  const char *font_bold_path = NULL;
+  const char *cfdi = NULL;
+  const char *cfdi_pdf = NULL;
+  int font_size = 8;
+  int font_label_size = 8;
+  int want_help = 0;
+  int want_verbose = 0;
 
-  if ( argc == 2 ) {
-    /* En este caso, solo fue dado el CFDI y el archivo para la
-     * representacion impresa sera tomado del nombre del archivo que contiene
-     * el CFDI, pero ahora con extension .pdf
-     */
+  /* options descriptor */
+  static struct option longopts[] = {
+	{"banner",		required_argument,	NULL,	'b'},
+	{"info",		required_argument,	NULL,	'i'},
+	{"font",		required_argument,	NULL,	'f'},
+	{"font-bold",		required_argument,	NULL,	'F'},
+	{"font-size",		required_argument,	NULL,	's'},
+	{"font-label-size",	required_argument,	NULL,	'l'},
+	{"verbose",		no_argument,		NULL,	'v'},
+	{"help",		no_argument,		NULL,	'h'},
+	{NULL,			0,			NULL,	 0 }
+  };
+
+  while ((ch=getopt_long(argc,argv,"b:i:f:F:s:l:vh", longopts, NULL)) != -1 ) {
+
+    switch(ch) {
+
+      case 'b':
+	/* La leyenda de banner del comprobante */
+	banner = optarg;
+	break;
+
+      case 'i':
+	/* Info a mostrar en la factura, telefonos, email, etc */
+	info = optarg;
+	break;
+
+      case 'f':
+	/* El path de la font */
+	font_path = optarg;
+	break;
+
+      case 'F':
+	/* El path de la font bold */
+	font_bold_path = optarg;
+	break;
+
+      case 's':
+	/* El tamaño de la font en dpi, default 8 dpi */
+	font_size = atoi(optarg);
+	break;
+
+      case 'l':
+	/* El tamaño de la font en dpi, para las etiquetas de la informacion */
+	font_label_size = atoi(optarg);
+	break;
+
+      case 'v':
+	/* Activa el modo verbose */
+	want_verbose = 1;
+	break;
+
+      case 'h':
+	/* help */
+	want_help = 1;
+	break;
+
+      case '?':
+	/* getopt_long already printed an error message. */
+	break;
+
+      default:
+	break;
+
+    }
   }
 
-  if ( argc < 3 ) {
-    fprintf(stderr, "uso: %s CFDI PDF-FILE\n", argv[0]);
+  /* Check if help was selected */
+  if ( want_help ) {
+    usage();
+    return 0;
+  }
+
+  /* Ahora, preparate para leer algunos argumento posicionales
+   * los argumentos seran:
+   *
+   *		- el CFDI
+   *		- y, opcionalmente el nombre del archivo de salida de
+   *		  la representacion impresa
+   *
+   * En caso de que el nombre del archivo, en el que se escribira la
+   * la representacion impresa, no sea suministrado, el nombre se tomará
+   * del nombre del CFDI, pero ahora con la extensión pdf
+   */
+  if ( optind >= argc ) {
+    /* Esta es una situacion inusual, no fue proporcionado el CFDI, de
+     * modo que no es posible general la representacion impresa
+     */
+    fprintf(stderr, "Error. No fue propocionado la ruta del CFDI, de modo que no hay representación impresa a generar.\n");
+    usage();
     return 1;
   }
 
-  res = r12nimpresa(argv[1], argv[2], "Nombre del establecimiento",
-		    "Teléfonos: (xxx) xxx xxxx. email: ventas@example.org",
-		    NULL, NULL, verbose);
+  /* Lee al argumento */
+  if ( argv[optind] != NULL ) {
+    cfdi = argv[optind++];
+  }
+
+  /* Verifica si fue proporcionado el archivo de salida */
+  if ( argv[optind] != NULL ) {
+    cfdi_pdf = argv[optind];
+
+  } else {
+    /* Genera el nombre del archivo de salida */
+
+  }
+
+  res = r12nimpresa(cfdi, cfdi_pdf, banner, info, font_path, font_bold_path,
+		    want_verbose);
 
   return res;
 }
